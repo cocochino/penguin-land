3ac09fa4c324f2d751300929a75e21b3ac779fad
unknown <cocochino@gmail.com>
2023-09-14 14:28:32 -0400

    Modified POSTMAN test cases

--- tests/browser/test_wait_implicit.py
+++ tests/browser/test_wait_implicit.py
@@ -2,15 +2,19 @@
 Created on Sep 11, 2023
 
 @author: Miho
+
+add waits to asynchronous pages
+When a webpage is loaded asynchronously, some elements take longer than others to load. 
+Waits add a time interval between actions performed by the WebDriver.
 '''
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-
+driver.implicitly_wait(5)
 
 driver.get("http://www.python.org")
 
 myelem = driver.find_element_by_id('start-shell')
-driver.implicitly_wait(5)
+
 
 driver.close()
