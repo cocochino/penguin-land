a334afe93c64dd033f2398a88afdf856b98c16db
unknown <cocochino@gmail.com>
2023-09-16 18:54:48 -0400

    Python-Selenium with assertion initial commit

---
+++ tests/assertion/sanity_open_FF.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 10, 2023
+
+@author: Miho
+
+geckodriver path has been added to OS environmental variable
+'''
+from selenium import webdriver;
+#from unittest import TestCase
+
+class sanityCheck():
+    
+        driver= webdriver.Firefox();
+        
+        driver.get('http://www.google.com');
+        
+        #self.assertEqual("Mooshu", "Mooshu")
+        driver.close()
+        '''def test_1(self):
+        driver= webdriver.Firefox();
+        
+        driver.get('http://www.google.com');
+        
+        #self.assertEqual("Mooshu", "Mooshu")
+        driver.close()
+    '''
---
+++ tests/assertion/test_string_comparison.py
@@ -0,0 +1,23 @@
+'''
+Created on Sep 10, 2023
+
+@author: Miho
+
+Assertion example - Run this as PyUnit
+'''
+from selenium import webdriver;
+from unittest import TestCase
+
+class sanityCheck(TestCase):
+    
+    def setUp(self):
+        self.driver= webdriver.Firefox();
+        self.driver.get('http://www.google.com');
+        
+    def test_one(self):
+        self.assertEqual("Mooshu", "Mooshu")
+        #self.driver.close()
+
+    def tearDown(self):
+        self.driver.close()
+        
