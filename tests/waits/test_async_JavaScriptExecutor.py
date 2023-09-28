61de97d6b8564bf3ebe42a7fb4df51a20734b4be
unknown <cocochino@gmail.com>
2023-09-28 13:18:16 -0400

    Removed unused libraries.

--- tests/waits/test_async_JavaScriptExecutor.py
+++ tests/waits/test_async_JavaScriptExecutor.py
@@ -4,11 +4,9 @@ Created on Sep 27, 2023
 @author: Miho
 
 Utilize JavaScriptExecutor
+In Python, jsReturnsValue, etc can be done using drive.execute_script
 '''
 from selenium import webdriver;
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from unittest import TestCase
 
 
