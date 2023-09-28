4855a1cce8531dfd11b05b662659529cb79b0239
unknown <cocochino@gmail.com>
2023-09-28 13:18:49 -0400

    How to wait for slow page that uses JavaScript. Initial checkin.

---
+++ tests/waits/test_JavaScript_slow_page.py
@@ -0,0 +1,35 @@
+'''
+Created on Sep 28, 2023
+
+@author: Miho
+
+Working with slow loading page that uses JS
+When the expected message can't be found, it returns timeout exception.
+'''
+from selenium import webdriver;
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from unittest import TestCase
+
+class waitForSlowJSPage(TestCase):
+    
+    def setUp(self):
+        self.driver= webdriver.Firefox()
+        
+    def test_one(self):
+        self.driver.get('https://eviltester.github.io/synchole/messages.html')
+        
+        ''' Wait for the JavaScript finish loading messages 
+        Step 1. Use expected condition to check for the presence of process completion message
+        Step 2. Use JS Executor to check that all process is done. '''
+        WebDriverWait(self.driver, 15).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, 'messagecount'),'Message Count: 0 : 0'
+            )
+        )
+        page_status = self.driver.execute_script("return (window.totalMessagesReceived>0 && window.renderingQueueCount==0)")
+        self.assertTrue(page_status,'JavaScript has not been completed.')
+
+    def tearDown(self):
+        self.driver.close()
